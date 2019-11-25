f95f37bb010bd19c266241e25b26dd060bde886e
The Cryptic Canadian <ecrann@protonmail.com>
2019-11-25 17:25:44 -0330

    Cleaned code a small amount

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
 
 //Requirements for weather
 const DarkSky = require('dark-sky')
@@ -9,7 +9,7 @@ const { darkSkyKey } = require('./auth.json');
 //Requirements for google sheets
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 const TOKEN_PATH = 'token.json';
 
@@ -24,9 +24,9 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
 });
 
 //Basic Logs
@@ -39,150 +39,129 @@ bot.on('ready', function (evt) {
 
 //Reactions to messages
 bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 5) == 'Marvin') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
+    if (message.substring(0, 6) == 'Marvin') {
+        let args = message.split(' ');
+        let cmd = args[1];
+
         args = args.splice(1);
-        switch(cmd) {
+
+        switch (cmd) {
             // !ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!!!'
+                    message: 'What\'s the point? Am I a joke to you?'
                 });
             break;
-        
-         }
-     }
-
-    //
-    if(message == 'Marvin What is the answer to life, the universe, and everything?'){
-        bot.sendMessage({
-            to:channelID,
-                message: '42'
-            });
-        ; 
-    }
-    if(message.substring(0,10) == 'Marvin babblefish') {
-    //Potentially some for of translation?
-        bot.sendMessage({
-            to:channelID,
-                message: 'The poor Babel fish by effectively removing all barriers to communication between different races and cultures, has caused more and bloodier wars than anything else in the history of creation'
-        });
-    ; 
-    }
-
-    if(message.substring(0,10) == 'Marvin sheets'){
-        //Implments a function to read mixer stats from a google sheet
-
-        //Authorize function for sheets
-        function authorize(credentials, callback) {
-            const {client_secret, client_id, redirect_uris} = credentials.installed;
-            const oAuth2Client = new google.auth.OAuth2(
-                client_id, client_secret, redirect_uris[0]);
-        
-            // Check if we have previously stored a token.
-            fs.readFile(TOKEN_PATH, (err, token) => {
-            if (err) return getNewToken(oAuth2Client, callback);
-            oAuth2Client.setCredentials(JSON.parse(token));
-            return callback(oAuth2Client);
-            });
-        }
-
-        //Get new Token for google sheets  
-        function getNewToken(oAuth2Client, callback) {
-            const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: SCOPES,
-            });
-            console.log('Authorize this app by visiting this url:', authUrl);
-            const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-            });
-            rl.question('Enter the code from that page here: ', (code) => {
-            rl.close();
-            oAuth2Client.getToken(code, (err, token) => {
-                if (err) return console.error('Error while trying to retrieve access token', err);
-                oAuth2Client.setCredentials(token);
-                // Store the token to disk for later program executions
-                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
+            case 'babblefish':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'The poor Babel fish by effectively removing all barriers to communication between different races and cultures, has caused more and bloodier wars than anything else in the history of creation'
                 });
-                callback(oAuth2Client);
-            });
-            });
-        }
-        
+            break;
+            case 'sheets':
+                function authorize(credentials, callback) {
+                    const { client_secret, client_id, redirect_uris } = credentials.installed;
+                    const oAuth2Client = new google.auth.OAuth2(
+                        client_id, client_secret, redirect_uris[0]);
 
-
-
-
-        
-        //@param {google.auth.OAuth2} auth The authenticated Google OAuth client.
-        async function listMixerStats(auth) {
-            const sheets = google.sheets({version: 'v4', auth});
-            await sheets.spreadsheets.values.get({
-                spreadsheetId: '1yObdyGAsbbpAc8GFut0TSJ-1Tg4ghGt2snCs6i0zK_w',
-                range: 'Stats Sheet!A:A',
-                }, 
-                (err, res) => {
-                    //if (err) return console.log('The API returned an error: ' + err);
-                    try{
-                    const rows = res.data.values;
-                    lastMix = rows[0][0]+" "+rows[1][0];
-                    totalCash = rows[3][0]+" "+rows[4][0];
-                    cashLast = rows[6][0]+" "+rows[7][0];
-                    let outString = lastMix+"\n"+totalCash+"\n"+cashLast;
-                    console.log(outString);
-                    bot.sendMessage({
-                        to:channelID,
-                            message: outString
+                    // Check if we have previously stored a token.
+                    fs.readFile(TOKEN_PATH, (err, token) => {
+                        if (err) return getNewToken(oAuth2Client, callback);
+                        oAuth2Client.setCredentials(JSON.parse(token));
+                        return callback(oAuth2Client);
                     });
-                    }
-                    catch(err){
-                        return console.log('The API returned an error: ' + err);
-                    }
+                }
+                //Get new Token for google sheets  
+                function getNewToken(oAuth2Client, callback) {
+                    const authUrl = oAuth2Client.generateAuthUrl({
+                        access_type: 'offline',
+                        scope: SCOPES,
+                    });
+                    console.log('Authorize this app by visiting this url:', authUrl);
+                    const rl = readline.createInterface({
+                        input: process.stdin,
+                        output: process.stdout,
+                    });
+                    rl.question('Enter the code from that page here: ', (code) => {
+                        rl.close();
+                        oAuth2Client.getToken(code, (err, token) => {
+                            if (err) return console.error('Error while trying to retrieve access token', err);
+                            oAuth2Client.setCredentials(token);
+                            // Store the token to disk for later program executions
+                            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                                if (err) return console.error(err);
+                                console.log('Token stored to', TOKEN_PATH);
+                            });
+                            callback(oAuth2Client);
+                        });
+                    });
+                }
+                //@param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+                async function listMixerStats(auth) {
+                    const sheets = google.sheets({ version: 'v4', auth });
+                    await sheets.spreadsheets.values.get({
+                        spreadsheetId: '1yObdyGAsbbpAc8GFut0TSJ-1Tg4ghGt2snCs6i0zK_w',
+                        range: 'Stats Sheet!A:A',
+                    },
+                        (err, res) => {
+                            //if (err) return console.log('The API returned an error: ' + err);
+                            try {
+                                const rows = res.data.values;
+                                lastMix = rows[0][0] + " " + rows[1][0];
+                                totalCash = rows[3][0] + " " + rows[4][0];
+                                cashLast = rows[6][0] + " " + rows[7][0];
+                                let outString = lastMix + "\n" + totalCash + "\n" + cashLast;
+                                console.log(outString);
+                                bot.sendMessage({
+                                    to: channelID,
+                                    message: outString
+                                });
+                            }
+                            catch (err) {
+                                return console.log('The API returned an error: ' + err);
+                            }
+                        });
+                }
+                // Load client secrets from a local file.
+                fs.readFile('credentials.json', async (err, content) => {
+                    if (err) return console.log('Error loading client secret file:', err);
+                    // Authorize a client with credentials, then call the Google Sheets API.
+                    authorize(JSON.parse(content), listMixerStats);
                 });
+            break;
+            case 'weather':
+                let darksky = new DarkSky(darkSkyKey);
+                (async () => {
+                    const weather = await darksky.latitude('47.5165').longitude('-52.7126').get().catch(console.log)
+                    console.log(weather);
+
+                    console.log(weather.timezone);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Brain the size of the planet and you ask me the weather?' + "\n" +
+                            "Weather Summary: " + weather.currently.summary + "\n" +
+                            "Temperature: " + weather.currently.apparentTemperature + "\n" +
+                            "Apparent Temperature: " + weather.currently.temperature + "\n" +
+                            "Precipitation Probability: " + weather.currently.precipProbability + "\n" +
+                            "Precipitation Amount: " + weather.currently.precipIntensity + "\n" +
+                            "Wind Speed: " + weather.currently.windSpeed + "\n"
+                    });
+                })();
+            break;
+
         }
-
-
-        // Load client secrets from a local file.
-        fs.readFile('credentials.json', async (err, content) => {
-            if (err) return console.log('Error loading client secret file:', err);
-                // Authorize a client with credentials, then call the Google Sheets API.
-                authorize(JSON.parse(content), listMixerStats);
+    }
+    if (message == 'Marvin what is the answer to life, the universe, and everything?') {
+        bot.sendMessage({
+            to: channelID,
+            message: '42'
         });
-
-
-  
-
     }
 
-    //Dark Sky Weather Implementation
-    if(message.substring(0,15) == 'Marvin weather'){
-        let darksky = new DarkSky(darkSkyKey);
-        (async () => {
-            const weather = await darksky.latitude('47.5165').longitude('-52.7126').get().catch(console.log)
-            console.log(weather);
 
-            console.log(weather.timezone);
-            bot.sendMessage({
-                to:channelID,
-                    message: 'Brain the size of the planet and you ask me the weather?'+"\n"+
-                    "Weather Summary: "+weather.currently.summary+"\n"+
-                    "Temperature: "+weather.currently.apparentTemperature+"\n"+
-                    "Apparent Temperature: "+weather.currently.temperature+"\n"+
-                    "Precipitation Probability: "+weather.currently.precipProbability+"\n"+
-                    "Precipitation Amount: "+weather.currently.precipIntensity+"\n"+
-                    "Wind Speed: "+weather.currently.windSpeed+
-                    "Powered by Dark Sky -> https://darksky.net/poweredby/"
-            });
-        })();
 
-    }
+
 });
 
 
